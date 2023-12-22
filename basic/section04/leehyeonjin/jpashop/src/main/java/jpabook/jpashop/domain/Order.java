ed4b1e9ecff2af6581a7bb775ed117742d72b88a
hgene0929@gmail.com <hgene0929@gmail.com>
2023-12-22 16:40:37 +0900

    [docs] section04 documentation & initialization

---
+++ basic/section04/leehyeonjin/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "order_id")
+	private Long id;
+
+	@Column(name = "member_id")
+	private Long memberId;
+
+	@Column(name = "order_date")
+	private LocalDateTime orderDate; // Java의 카멜표기법으로 표시해도, spring boot 사용시 알아서 DB의 관례대로 언더스코어 표기법으로 DB에 저장
+
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+}
